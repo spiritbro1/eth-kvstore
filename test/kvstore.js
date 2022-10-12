64e656cae842420d20984c5f9ebcfa06de946222
ꦫꦶꦤꦺꦴ <62529025+spiritbro1@users.noreply.github.com>
2022-10-12 08:06:10 +0700

    Update kvstore.js

--- test/kvstore.js
+++ test/kvstore.js
@@ -2,11 +2,20 @@ const KVStore = artifacts.require('./KVStore.sol');
 const openpgp = require('openpgp');
 const fs = require('fs');
 const path = require('path');
+const { assert } = require('chai');
+const truffleAssert = require('truffle-assertions');
+
+let instance;
+let accountOne;
+let accountTwo;
 
 contract('KVStore', async (accounts) => {
+  before(async () => {
+    instance = await KVStore.deployed();
+    [accountOne, accountTwo] = accounts;
+  });
+
   it('returns a correct value to the address storing the key-value pair', async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne] = accounts;
     await instance.set('satoshi', 'nakamoto', {
       from: accountOne,
     });
@@ -17,8 +26,6 @@ contract('KVStore', async (accounts) => {
   });
 
   it('returns a correct value to another address', async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne, accountTwo] = accounts;
     await instance.set('satoshi', 'nakamoto', {
       from: accountOne,
     });
@@ -29,106 +36,73 @@ contract('KVStore', async (accounts) => {
   });
 
   it("doesn't allow storing a too long key", async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne] = accounts;
-    try {
-      await instance.set(
+    await truffleAssert.reverts(
+      instance.set(
         'satoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamoto',
         'satoshi',
         { from: accountOne },
-      );
-      assert.fail();
-    } catch (err) {
-      assert.ok(/revert/.test(err.message));
-    }
+      ),
+      '',
+    );
   });
 
   it("doesn't allow storing a too long value", async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne] = accounts;
-    try {
-      await instance.set(
+    await truffleAssert.reverts(
+      instance.set(
         'satoshi',
         'satoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamoto',
         { from: accountOne },
-      );
-      assert.fail();
-    } catch (err) {
-      assert.ok(/revert/.test(err.message));
-    }
+      ),
+      '',
+    );
   });
 
   it('outputs an address on deployment', async () => {
-    const instance = await KVStore.deployed();
     assert.typeOf(instance.address, 'string');
     assert.isAtLeast(instance.address.length, 10);
   });
 
   it('store public key and its fingerprint on smart contract', async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne] = accounts;
-    try {
-      await instance.setPGP(
-        '71f8b7a5fcd42ddc8fe5b89ae536191d60754345',
-        'bafkreieadxyvobae4a2ww7tytw37dw2ihvvbujj5npjepurraavtaoczcq',
-        { from: accountOne },
-      );
+    await instance.setPGP(
+      '71f8b7a5fcd42ddc8fe5b89ae536191d60754345',
+      'bafkreieadxyvobae4a2ww7tytw37dw2ihvvbujj5npjepurraavtaoczcq',
+      { from: accountOne },
+    );
 
-      const value = await instance.getAllPGP.call(accountOne, {
-        from: accountOne,
-      });
-      assert.equal(value[0], 'bafkreieadxyvobae4a2ww7tytw37dw2ihvvbujj5npjepurraavtaoczcq');
-    } catch (err) {
-      assert.ok(/revert/.test(err.message));
-    }
+    const value = await instance.getAllPGP.call(accountOne, {
+      from: accountOne,
+    });
+    assert.equal(value[0], 'bafkreieadxyvobae4a2ww7tytw37dw2ihvvbujj5npjepurraavtaoczcq');
   });
+
   it('shouldn\'t store same public key fingerprint multiple times', async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne] = accounts;
-    try {
-      await instance.setPGP(
+    await truffleAssert.reverts(
+      instance.setPGP(
         '71f8b7a5fcd42ddc8fe5b89ae536191d60754345',
         'bafkreieadxyvobae4a2ww7tytw37dw2ihvvbujj5npjepurraavtaoczcq',
         { from: accountOne },
-      );
-      await instance.setPGP(
-        '71f8b7a5fcd42ddc8fe5b89ae536191d60754345',
-        'bafkreieadxyvobae4a2ww7tytw37dw2ihvvbujj5npjepurraavtaoczcq',
-        { from: accountOne },
-      );
-      const value = await instance.getAllPGP.call(accountOne, {
-        from: accountOne,
-      });
-      assert.equal(value.length, 1);
-      assert.fail();
-    } catch (err) {
-      assert.ok(/revert/.test(err.message));
-    }
+      ),
+      'pgp already used',
+    );
   });
 
   it('should store encrypted value and decrypt the value', async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne] = accounts;
-    try {
-      const encrypted = fs.readFileSync(path.resolve(__dirname, './encrypted_message.txt'), { encoding: 'utf8', flag: 'r' });
-      const privkey = fs.readFileSync(path.resolve(__dirname, './private_key_for_testing'), { encoding: 'utf8', flag: 'r' });
-      await instance.set('satoshi', encrypted, {
-        from: accountOne,
-      });
-      const value = await instance.get.call(accountOne, 'satoshi', {
-        from: accountOne,
-      });
-      assert.equal(value, encrypted);
-      const message = await openpgp.readMessage({
-        armoredMessage: encrypted,
-      });
-      const { data: decrypted } = await openpgp.decrypt({
-        message,
-        decryptionKeys: await openpgp.readPrivateKey({ armoredKey: privkey }),
-      });
-      assert.equal(decrypted, 'H');
-    } catch (err) {
-      assert.ok(/revert/.test(err.message));
-    }
+    const encrypted = fs.readFileSync(path.resolve(__dirname, './encrypted_message.txt'), { encoding: 'utf8', flag: 'r' });
+    const privkey = fs.readFileSync(path.resolve(__dirname, './private_key_for_testing'), { encoding: 'utf8', flag: 'r' });
+    await instance.set('satoshi', encrypted, {
+      from: accountOne,
+    });
+    const value = await instance.get.call(accountOne, 'satoshi', {
+      from: accountOne,
+    });
+    assert.equal(value, encrypted);
+    const message = await openpgp.readMessage({
+      armoredMessage: encrypted,
+    });
+    const { data: decrypted } = await openpgp.decrypt({
+      message,
+      decryptionKeys: await openpgp.readPrivateKey({ armoredKey: privkey }),
+    });
+    assert.equal(decrypted, 'H');
   });
 });
